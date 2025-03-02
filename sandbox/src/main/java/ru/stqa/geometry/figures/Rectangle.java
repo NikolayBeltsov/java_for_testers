35e1fcb9afaff73581ce64f4c97704f9c0173341
Nikolay.B <n.beltsov@yandex.ru>
2025-03-02 17:14:40 +0300

    Ветвление (if, then, else)

--- sandbox/src/main/java/ru/stqa/geometry/figures/Rectangle.java
+++ sandbox/src/main/java/ru/stqa/geometry/figures/Rectangle.java
@@ -2,12 +2,18 @@ package ru.stqa.geometry.figures;
 
 public record Rectangle(double a, double b) {
 
-    public static void printRectangleArea(double a, double b) {
-        var text = String.format("Площадь прямоугольника со сторонами %f и %f = %f", a, b, rectangleArea(a, b));
-        System.out.println(text);
+    public Rectangle {
+        if (a < 0 || b < 0) {
+            throw new IllegalArgumentException("Rectangle side should be non-negative");
+        }
     }
+    public static void printRectangleArea(double a, double b) {
+            var text = String.format("Площадь прямоугольника со сторонами %f и %f = %f", a, b, rectangleArea(a, b));
+            System.out.println(text);
+        }
 
     private static double rectangleArea(double a, double b) {
-        return a * b;
-    }
+            return a * b;
+        }
+
 }
