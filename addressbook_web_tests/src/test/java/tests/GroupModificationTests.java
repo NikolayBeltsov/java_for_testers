a1c2fe44c198b0fed618388d115d14c1474018d8
Nikolay.B <n.beltsov@yandex.ru>
2025-03-25 20:18:54 +0300

    Сравнение списков

--- addressbook_web_tests/src/test/java/tests/GroupModificationTests.java
+++ addressbook_web_tests/src/test/java/tests/GroupModificationTests.java
@@ -9,7 +9,7 @@ public class GroupModificationTests extends TestBase {
     void canModifyGroup() {
         //Если нет группы, то создается новая
         if (app.groups().getCount() == 0) {
-            app.groups().createGroup(new GroupData("group name", "group header", "group footer"));
+            app.groups().createGroup(new GroupData("", "group name", "group header", "group footer"));
         }
         app.groups().modifyGroup(new GroupData().withName("modified name"));
     }
--- addressbook_web_tests/src/test/java/tests/GroupRemovalTests.java
+++ addressbook_web_tests/src/test/java/tests/GroupRemovalTests.java
@@ -4,19 +4,26 @@ import model.GroupData;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class GroupRemovalTests extends TestBase {
 
     @Test
     public void canRemoveGroup() {
         //Если нет группы, то создается новая
         if (app.groups().getCount() == 0) {
-            app.groups().createGroup(new GroupData("group name", "group header", "group footer"));
+            app.groups().createGroup(new GroupData("", "group name", "group header", "group footer"));
         }
-        int groupCount = app.groups().getCount();
+        var oldGroups = app.groups().getList();
+        var rnd = new Random();
+        var index = rnd.nextInt(oldGroups.size());
         //Удаляем группу
-        app.groups().removeGroup();
-        int newgroupCount = app.groups().getCount();
-        Assertions.assertEquals(groupCount - 1, newgroupCount);
+        app.groups().removeGroup(oldGroups.get(index));
+        var newGroups = app.groups().getList();
+        var expectedList = new ArrayList<>(oldGroups);
+        expectedList.remove(index);
+        Assertions.assertEquals(newGroups, expectedList);
 
     }
 
@@ -25,7 +32,7 @@ public class GroupRemovalTests extends TestBase {
     void canRemoveAllGroupsAtOnce() {
         //Если нет группы, то создается новая
         if (app.groups().getCount() == 0) {
-            app.groups().createGroup(new GroupData("group name", "group header", "group footer"));
+            app.groups().createGroup(new GroupData("", "group name", "group header", "group footer"));
         }
         app.groups().removeAllGroups();
         Assertions.assertEquals(0, app.groups().getCount());
