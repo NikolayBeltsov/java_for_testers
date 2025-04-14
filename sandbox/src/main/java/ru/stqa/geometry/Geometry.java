b5ef2d6da27b864edc62b2f688ba32e64d796967
Nikolay.B <n.beltsov@yandex.ru>
2025-04-14 21:13:04 +0300

    Функциональный стиль программирования

--- sandbox/src/main/java/ru/stqa/geometry/Geometry.java
+++ sandbox/src/main/java/ru/stqa/geometry/Geometry.java
@@ -1,25 +1,21 @@
 package ru.stqa.geometry;
 
+import ru.stqa.geometry.figures.Rectangle;
+import ru.stqa.geometry.figures.Square;
 import ru.stqa.geometry.figures.Triangle;
+
+import java.util.List;
+import java.util.function.Consumer;
 
 public class Geometry {
     public static void main(String[] args) {
-        //Задаем параметры треугольника
-        Triangle myTriangle = new Triangle(5.0, 7.0, 5.0);
-        /*
-        Square.printSquareArea(new Square(7.0));
-        Square.printSquareArea(new Square(5.0));
-        Square.printSquareArea(new Square(3.0));
+        var squares = List.of(new Square(7.0), new Square(5.0), new Square(3.0));
+//        for (Square square : squares) {
+//            Square.printSquareArea(square);
+//        }
+        squares.forEach(Square::printSquareArea);
 
-        Rectangle.printRectangleArea(3.0, 5.0);
-        Rectangle.printRectangleArea(7.0, 9.0);
-        */
-
-//Вывод результата по Периметру треугольника
-        myTriangle.printTrianglePerimeter();
-
-//Вывод результата по Площади треугольника
-        myTriangle.printTriangleArea();
+    //    Rectangle.printRectangleArea(3.0, 5.0);
+    //    Rectangle.printRectangleArea(7.0, 9.0);
     }
-
 }
