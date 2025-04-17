46ff92fa5cc587410ea430229af4022dec7fe01a
Nikolay.B <n.beltsov@yandex.ru>
2025-04-17 21:26:36 +0300

    Домашнее задание №17
    Подготовить новый проект для нового учебного приложения

---
+++ mantis_tests/src/test/java/ru/stqa/mantis/manager/HelperBase.java
@@ -0,0 +1,37 @@
+package ru.stqa.mantis.manager;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.nio.file.Paths;
+import java.time.Duration;
+
+public class HelperBase {
+    protected final ApplicationManager manager;
+
+    public HelperBase(ApplicationManager manager) {
+        this.manager = manager;
+    }
+
+    protected void click(By locator) {
+        WebDriverWait wait = new WebDriverWait(manager.driver(), Duration.ofSeconds(3));
+        WebElement element = wait.until(ExpectedConditions.elementToBeClickable(locator));
+        element.click();
+    }
+
+    protected void type(By locator, String text) {
+        click(locator);
+        manager.driver().findElement(locator).clear();
+        manager.driver().findElement(locator).sendKeys(text);
+    }
+
+    protected void attach(By locator, String file) {
+        manager.driver().findElement(locator).sendKeys(Paths.get(file).toAbsolutePath().toString());
+    }
+
+    protected boolean isElementPresent(By locator) {
+        return manager.driver().findElements(locator).size() > 0;
+    }
+}
